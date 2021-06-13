c315a2c5ab0cbb93cf8da9e350e22809906c1693
DenisMurashko <dennn87@mail.ru>
2021-06-13 20:30:18 +0300

    Add test for contract service

--- eCare/src/test/java/services/ContractServiceImpTest.java
+++ eCare/src/test/java/services/ContractServiceImpTest.java
@@ -2,6 +2,7 @@ package services;
 
 import com.t_systems.ecare.eCare.DAO.*;
 import com.t_systems.ecare.eCare.DTO.ContractDTO;
+import com.t_systems.ecare.eCare.basket.BasketImpl;
 import com.t_systems.ecare.eCare.entity.Contract;
 import com.t_systems.ecare.eCare.entity.Customer;
 import com.t_systems.ecare.eCare.entity.Option;
@@ -13,10 +14,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.commons.util.CollectionUtils;
+import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
@@ -24,9 +27,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -34,6 +35,7 @@ import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.WARN)
+@RunWith(MockitoJUnitRunner.class)
 class ContractServiceImpTest {
     @Mock
     ContractDAOImpl contractDAO;
@@ -55,60 +57,75 @@ class ContractServiceImpTest {
     ContractServiceImp contractServiceImp;
     @Mock
     Tariff tariff;
+    @Mock
+    Contract contract;
+    @Mock
+    Customer customer;
+    @Mock
+    ContractDTO contractDTO;
 
     @BeforeEach
     void before() {
-        //enable mocks
         MockitoAnnotations.initMocks(this);
     }
 
     @Test
     void update() {
-        //when(mock).thenReturn(value)
         ContractDTO dto = new ContractDTO();
         dto.setId(1);
-        Contract contract = mock(Contract.class);
-        when(tariffDAO.findOne(anyInt()))
-                .thenReturn(tariff);
-        when(contractDAO.findOne(anyInt()))
-                .thenReturn(contract);
+        Set<Integer> idOptionsForAdd = new HashSet<>();
+        idOptionsForAdd.add(1);
+        dto.setOptionsIds(idOptionsForAdd);
+        dto.setTariffName("First");
+        List<Option> optionList = new ArrayList<>();
+        Option newOption = new Option();
+        newOption.setName("First");
+        newOption.setId(1);
+        optionList.add(newOption);
+        Contract newContract = mock(Contract.class);
+        when(tariffDAO.findOne(anyInt())).thenReturn(tariff);
+        when(contractDAO.findOne(anyInt())).thenReturn(contract);
         when(optionService.checkCompatibilityOptions(anySet(), anyList(), anySet())).thenReturn(Optional.empty());
         Set<String> optionsNames = new HashSet<>();
         optionsNames.add("Internet");
-        when(optionService.deleteOptionsAvailableTariffAnDADDNameOption(anySet(), anyList())).thenReturn(optionsNames);
+        when(optionService.deleteOptionsAvailableTariffAnDADDNameOption(idOptionsForAdd, optionList)).thenReturn(optionsNames);
         Customer customer = mock(Customer.class);
         when(contract.getCustomerId()).thenReturn(customer);
-     //   ArgumentCaptor<Contract> contractArgumentCaptor = ArgumentCaptor.forClass(Contract.class);
-
-     //   verify(contractDAO).update(contractArgumentCaptor.capture());
-    //    Contract ubdateContract = contractArgumentCaptor.getValue();
-
-        assertEquals(optionsNames, dto.getAddNameOptions());
-  //      assertEquals(ubdateContract.getCustomerId(),customer);
+        when(contractServiceImp.convertToEntity(dto)).thenReturn(newContract);
+        Optional<String> e = contractServiceImp.update(dto);
+        assertFalse(e.isPresent());
     }
 
     @Test
-    void getDto() {
-    }
+    void updateOptionСompatibilityError() {
 
-    @Test
-    void showTariffandOptions() {
+        assertEquals(optionService.checkCompatibilityOptions(anySet(), anyList(), anySet()).isPresent(), false);
+
     }
 
     @Test
     void create() {
+        when(tariffDAO.findOne(anyInt())).thenReturn(tariff);
+        when(optionService.checkCompatibilityOptions(anySet(), anyList(), anySet())).thenReturn(Optional.empty());
+        Optional<String> e = contractServiceImp.create(contractDTO);
+        assertFalse(e.isPresent());
     }
 
     @Test
-    void showAllContracts() {
-    }
-
-    @Test
-    void isContractBlocked() {
+    void createOptionСompatibilityError() {
+        when(tariffDAO.findOne(anyInt())).thenReturn(tariff);
+        when(optionService.checkCompatibilityOptions(anySet(), anyList(), anySet())).thenReturn(Optional.of("Error"));
+        Optional<String> e = contractServiceImp.create(contractDTO);
+        assertTrue(e.isPresent());
     }
 
     @Test
     void updateByCart() {
+        BasketImpl basket = mock(BasketImpl.class);
+        when(tariffDAO.findOne(basket.getTariffId())).thenReturn(tariff);
+        when(contractDAO.findOne(basket.getId())).thenReturn(contract);
+        when(contractServiceImp.convertToDto(contract)).thenReturn(contractDTO);
+        assertEquals(basket.getNumber(),contractDTO.getNumber());
     }
 
 }
